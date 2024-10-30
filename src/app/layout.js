8c4179e6518cf38eaff45d7f3fd12a2960014ef5
alisoneoz <alisonestephanyoz@gmail.com>
2024-10-30 18:28:08 -0400

    Add Google Analytics tracking code to Next.js app layout
    • Integrate GA4 tracking script • Configure gtag initialization • Set afterInteractive loading strategy • Add script to head section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from 'next/script';
+import { GA_TRACKING_ID } from '@/lib/gtag';
 import Navbar from '@/components/Navigation/Navbar';
 import Footer from '@/components/Footer/Footer';
 
@@ -22,6 +24,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+            <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
