37bd218c6a5528b03655cfeb415d6fa61d7cf3b7
alisoneoz <alisonestephanyoz@gmail.com>
2025-06-25 22:05:04 -0400

    add bootcamp tech stack

--- src/config/technologies.js
+++ src/config/technologies.js
@@ -3,93 +3,92 @@ export const technologies = {
     {
       name: "JavaScript",
       icon: "/tech-icons/javascript-color.svg",
-      category: "core"
+      category: "core",
     },
     {
       name: "Python",
       icon: "/tech-icons/python.svg",
-      category: "core"
+      category: "core",
     },
     {
-      name: "Python",
-      icon: "/tech-icons/python.svg",
-      category: "core"
+      name: "SQL",
+      icon: "/tech-icons/SQL.svg",
+      category: "core",
     },
   ],
   frontend: [
     {
       name: "React",
       icon: "/tech-icons/react-color.svg",
-     
     },
     {
       name: "Next.js",
       icon: "/tech-icons/nextdotjs-color.svg",
-      
-    },
-    {
-      name: "TailwindCSS",
-      icon: "/tech-icons/tailwindcss-color.svg",
-      
     },
     {
       name: "Astro",
       icon: "/tech-icons/astro-color.svg",
-     
+    },
+    {
+      name: "TailwindCSS",
+      icon: "/tech-icons/tailwindcss-color.svg",
+    },
+    {
+      name: "Bootstrap",
+      icon: "/tech-icons/bootstrap.svg",
     },
     {
       name: "Redux Toolkit",
       icon: "/tech-icons/redux-color.svg",
-    
     },
     {
       name: "Material UI",
       icon: "/tech-icons/mui-color.svg",
-      
     },
     {
       name: "Shadcn/UI",
       icon: "/tech-icons/shadcnui-color.svg",
-     
     },
   ],
+  testing:[{
+    name:"Jest",
+    icon:"/tech-icons/jest.svg"
+  }],
   backend: [
     {
       name: "Flask",
-      icon: "/tech-icons/express-color.svg",
-      
+      icon: "/tech-icons/flask.svg",
+    },
+    {
+      name: "SQL Alchemy",
+      icon: "/tech-icons/sql-alchemy.svg",
     },
     {
       name: "Express.js",
       icon: "/tech-icons/express-color.svg",
-      
     },
     {
       name: "SQL",
       icon: "/tech-icons/SQL.svg",
-      
     },
     {
       name: "MongoDB",
       icon: "/tech-icons/mongodb-color.svg",
-      
     },
     {
-        name: "Mongoose",
-        icon: "/tech-icons/mongoosedotws-color.svg",
-       
-      },
+      name: "Mongoose",
+      icon: "/tech-icons/mongoosedotws-color.svg",
+    },
     {
       name: "Firebase",
       icon: "/tech-icons/firebase-color.svg",
-     
     },
   ],
   "CMS and Web Builders": [
     {
       name: "WordPress",
       icon: "/tech-icons/wordpress-color.svg",
-      features: ["Elementor", "Spectra"]
+      features: ["Elementor", "Spectra"],
     },
 
     {
@@ -103,7 +102,7 @@ export const technologies = {
     {
       name: "Framer",
       icon: "/tech-icons/framer.svg",
-    }
+    },
   ],
   tools: [
     {
@@ -117,7 +116,7 @@ export const technologies = {
     {
       name: "Canva Pro",
       icon: "/tech-icons/canva-color.svg",
-      features: ["AI features"]
+      features: ["AI features"],
     },
   ],
   ecommerce: [
@@ -135,5 +134,5 @@ export const technologies = {
       name: "Mailchimp",
       icon: "/tech-icons/mailchimp-color.svg",
     },
-  ]
+  ],
 };
