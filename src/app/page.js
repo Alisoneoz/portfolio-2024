262df24edd85307247ca80f616aab67dcb9d4d18
alisoneoz <alisonestephanyoz@gmail.com>
2025-08-05 23:16:39 -0400

    FIX: center heading

--- src/app/page.js
+++ src/app/page.js
@@ -99,8 +99,8 @@ export default function Home() {
 
         {/* Project Section */}
         <SectionTracker sectionName="projects">
-         
-            <h2 className="text-3xl font-bold text-center mb-8">Projects</h2>
+                
+            <h2 className="text-3xl font-bold text-center mb-16 overflow-hidden w-full ">Projects</h2>
             <ProjectsSection />
           
         </SectionTracker>
